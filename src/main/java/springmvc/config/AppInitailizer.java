11c772eb5592aaae2f23a377d76f9fb8f4b16dcb
iamneowise <wise@iamneo.ai>
2022-04-08 18:19:42 +0530

    spring mvc code

---
+++ src/main/java/springmvc/config/AppInitailizer.java
@@ -0,0 +1,30 @@
+package springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitailizer extends AbstractAnnotationConfigDispatcherServletInitializer{ {
+
+}
+
+@Override
+protected Class<?>[] getRootConfigClasses() {
+	
+	return new Class[] {
+			AppContext.class
+	};
+}
+
+@Override
+protected Class<?>[] getServletConfigClasses() {
+	
+	return new Class[] {
+			WebMvcConfig.class
+	};
+}
+
+@Override
+protected String[] getServletMappings() {
+	
+	return new String[] {"/"};
+}
+}
